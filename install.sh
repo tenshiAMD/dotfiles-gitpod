d26a8dddd23832b4f63efe05eebcc0092b4b2aa2
Angel Aviel Domaoan <dev.tenshiamd@gmail.com>
2022-01-26 23:36:33 +0800

    Setup essentials for development

--- install.sh
+++ install.sh
@@ -6,3 +6,8 @@ sudo ln -s ~/.dotfiles/.wgetrc ~/.wgetrc
 
 # Overwrite gitpod's existing config
 sudo rm ~/.gitconfig && sudo ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
+# Setup essentials for development
+sudo cp -Rn ~/.dotfiles/.vscode $THEIA_WORKSPACE_ROOT
+sudo cp -n ~/.dotfiles/.editorconfig $THEIA_WORKSPACE_ROOT/.editorconfig
+sudo cp -n ~/.dotfiles/.gitignore $THEIA_WORKSPACE_ROOT/.gitignore
